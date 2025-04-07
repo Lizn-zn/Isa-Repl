0e478494c7400ef42e17e9435677b38b6c562724
SoaringE <baodingh0@gmail.com>
2025-04-07 17:18:22 +0800

    modify .sbt

--- python-test/test_repl.py
+++ python-test/test_repl.py
@@ -8,20 +8,22 @@ gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25555))
 isa_repl = gateway.entry_point
         
 # Initialize REPL with a theory file
-theory_file = os.path.abspath("python-test/Test.thy")
-isa_repl.initializeRepl(theory_file)
+theory_file = os.path.abspath("Test.thy")
+
+
+isa_repl._initializeRepl(theory_file)
         
 # Compile the theory file
-result = isa_repl.compile()
+result = isa_repl._compile()
 print("Compilation result:", result)
         
 # Create and prove a theorem
 theorem = "lemma fixes x :: int shows \"x ^ 3 = x * x * x\" \n proof- \n"
-result = isa_repl.step(theorem)
+result = isa_repl._step(theorem)
 print("Theorem declaration result:", result)
         
 # Add a proof step
 proof_step = "show ?thesis by (simp add: numeral_eq_Suc)"
-result = isa_repl.step(proof_step)
+result = isa_repl._step(proof_step)
 print("Proof step result:", result)
         
