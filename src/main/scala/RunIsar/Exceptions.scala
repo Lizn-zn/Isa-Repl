5bf7aca9116392278f368941f5be2d19fdf2f1ed
Lizn-zn <lizenan1995@foxmail.com>
2025-04-18 17:00:57 +0000

    test

--- src/main/scala/RunIsar/Exceptions.scala
+++ src/main/scala/RunIsar/Exceptions.scala
@@ -2,6 +2,6 @@ package RunIsar
 
 import de.unruh.isabelle.control.IsabelleMLException
 
-object Exceptions {
+package object Exceptions {
   type IsabelleMLException = de.unruh.isabelle.control.IsabelleMLException
 } 
