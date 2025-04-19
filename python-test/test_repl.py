138e1d39ade2b02e6a3c12cd941b8e7ba2e457bb
Zenan Li <32972438+Lizn-zn@users.noreply.github.com>
2025-04-20 01:23:53 +0800

    Ci test (#12)
    * sbt minors & tweak check_nogoals
    
    * add interface of isa_reset
    
    * try_close finish
    
    * temp filemanage
    
    * roll back for timeout
    
    * simplify the apply strategy
    
    * update
    
    * WIP
    
    * WIP
    
    * update
    
    * Merge
    
    * tweak imports
    
    * tweak asScala.toList
    
    * redirect scala-isabelle
    
    * retrieve facts
    
    * retieve facts
    
    * WIP
    
    * update
    
    * update
    
    * WIP
    
    * change directory of sbt test
    
    * test
    
    * test ci
    
    * PATH update
    
    * chmod
    
    * Update ci.yml
    
    * test
    
    * test
    
    * update
    
    * add log
    
    * change working directory
    
    * update

--- python-test/test_repl.py
+++ python-test/test_repl.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-<<<<<<< HEAD
 import time
 import socket
 from py4j.java_gateway import JavaGateway, GatewayParameters
@@ -77,45 +76,3 @@ def test_repl():
             except subprocess.TimeoutExpired:
                 process.kill()
 
-=======
-from py4j.java_gateway import JavaGateway, GatewayParameters
-import pytest
-
-def test_repl():
-    jar_path = os.getenv("ISA_REPL_PATH")
-    process = subprocess.Popen([
-        "java", "-jar", jar_path, str(25555)
-    ])
-
-    # Connect to the JVM
-    gateway = JavaGateway(gateway_parameters=GatewayParameters(port=25555))
-            
-    # Get the IsaREPL application
-    isa_repl = gateway.entry_point
-            
-    # Initialize REPL with a theory file
-    theory_file = os.path.abspath("python-test/Test.thy")
-
-    isa_repl._initializeRepl(theory_file)
-            
-    # Compile the theory file
-    result = isa_repl._compile()
-    assert result == "True<\\SEP>"
-            
-    # Create and prove a theorem
-    theorem = "lemma fixes x :: int shows \"x ^ 3 = x * x * x\" \n proof- \n"
-    result = isa_repl._step(theorem)
-    assert "1. x ^ 3 = x * x * x" in result
-            
-    # Add a proof step
-    proof_step = "show ?thesis by (simp add: numeral_eq_Suc)"
-    result = isa_repl._step(proof_step)
-    assert "No subgoals!" in result
-            
-    # Close the REPL
-    isa_repl._exit()
-
-    # Terminate the Java process
-    process.terminate()
-
->>>>>>> bf05ee12ec682dd2a173bd0e58c68c6da7a1b577
