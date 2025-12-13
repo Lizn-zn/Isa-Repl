d456908d32e2d576814462c1ea2abc025bff0779
W Sun <114368189+Whatever314@users.noreply.github.com>
2025-12-14 04:15:56 +0800

    feat: add mill build configs for much better developing experience with metals.

---
+++ mill-build/src/Deps.scala
@@ -0,0 +1,13 @@
+package millbuild
+
+import mill.javalib.*
+
+object Deps {
+
+  val logbackClassic = mvn"ch.qos.logback:logback-classic:1.1.3"
+  val py4j = mvn"net.sf.py4j:py4j:0.10.9.7;exclude=org.slf4j:*"
+  val scalaIsabelle = mvn"de.unruh::scala-isabelle:0.4.3"
+  val scalaParserCombinators =
+    mvn"org.scala-lang.modules::scala-parser-combinators:2.1.1"
+  val scalatest = mvn"org.scalatest::scalatest:3.2.19"
+}
