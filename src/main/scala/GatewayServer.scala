5e2e5d7262a9d7e863868737589c26c3518fdbd4
W Sun <114368189+Whatever314@users.noreply.github.com>
2026-02-20 19:32:30 +0800

    fix error messages of `IsaReplApplication`'s step interfaces.

--- src/main/scala/GatewayServer.scala
+++ src/main/scala/GatewayServer.scala
@@ -109,7 +109,7 @@ class IsaReplApplication {
         "True" + "<\\SEP>" + repl.step(command)
       } catch {
         case e: IsabelleMLException =>
-          "False" + "<\\SEP>" + s"failed for prove the goal using the tactic `$command`. Get msg: ${e.getMessage}"
+          "False" + "<\\SEP>" + s"Failed applying command `$command`. Get msg: ${e.getMessage}"
       }
     result
   }
@@ -120,9 +120,9 @@ class IsaReplApplication {
         "True" + "<\\SEP>" + repl.step_with_30s(command)
       } catch {
         case e: IsabelleMLException =>
-          "False" + "<\\SEP>" + s"failed for prove the goal using the tactic `$command`. Get msg: ${e.getMessage}"
+          "False" + "<\\SEP>" + s"Failed to apply command `$command`. Get msg: ${e.getMessage}"
         case e: TimeoutException =>
-          "False" + "<\\SEP>" + s"failed for prove the goal using the tactic `$command`. Get msg: ${e.getMessage}"
+          "False" + "<\\SEP>" + s"Failed to apply command `$command`. Get msg: ${e.getMessage}"
       }
     result
   }
@@ -133,7 +133,7 @@ class IsaReplApplication {
         "True" + "<\\SEP>" + repl.step_without_timeout(command)
       } catch {
         case e: IsabelleMLException =>
-          "False" + "<\\SEP>" + s"failed for prove the goal using the tactic `$command`. Get msg: ${e.getMessage}"
+          "False" + "<\\SEP>" + s"Failed to apply command `$command`. Get msg: ${e.getMessage}"
       }
     result
   }
