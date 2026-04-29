d482c82d83d40f9bffc6fa5faa5bc0b799366027
W Sun <114368189+Whatever314@users.noreply.github.com>
2026-04-29 13:47:43 +0800

    mill build: use public scala-isabelle:0.4.3.

--- mill-build/src/Deps.scala
+++ mill-build/src/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
   val logbackClassic = mvn"ch.qos.logback:logback-classic:1.4.14"
   val logbackCore = mvn"ch.qos.logback:logback-core:1.4.14"
   val py4j = mvn"net.sf.py4j:py4j:0.10.9.7;exclude=org.slf4j:*"
-  val scalaIsabelle = mvn"de.unruh::scala-isabelle:master-SNAPSHOT"
+  val scalaIsabelle = mvn"de.unruh::scala-isabelle:0.4.3"
   val scalaParserCombinators =
     mvn"org.scala-lang.modules::scala-parser-combinators:2.1.1"
   val scalatest = mvn"org.scalatest::scalatest:3.2.19"
